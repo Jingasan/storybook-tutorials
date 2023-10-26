61f32431a11a03f0afee7616815fbca56428a940
Jinga <xxx@gmail.com>
2023-10-26 13:09:20 +0900

    <add> initial commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,15 @@
-import "./button.css";
-
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
-}
+import "./Button.css";
 
 /**
- * Primary UI component for user interaction
+ * ボタンコンポーネント
  */
+interface ButtonProps {
+  primary?: boolean; // UIのモードを「primary」にするか「secondary」にするか
+  backgroundColor?: string; // 背景色
+  size?: "small" | "medium" | "large"; // ボタンの大きさ「small」/「medium」/「large」
+  label: string; // ボタンに表示するテキスト
+  onClick?: () => void; // [Option] クリック時に動作させる関数
+}
 export const Button = ({
   primary = false,
   size = "medium",
